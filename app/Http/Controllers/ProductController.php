d09485e4744e3df347c6cae18002bcef67997fe2
Queensly Oko <quimaudongwo@gmail.com>
2022-08-10 15:28:20 +0100

    login and logout functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
-use Session;
+use Illuminate\Support\Facades\Session;
 
 class ProductController extends Controller
 {
